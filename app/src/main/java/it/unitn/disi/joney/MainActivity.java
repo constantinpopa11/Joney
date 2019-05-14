c09ff014dcd494ad9f88887e20804bd218924754
itLovaz <41294648+itLovaz@users.noreply.github.com>
2019-05-14 19:39:47 +0200

    Login and signup backend

--- app/src/main/java/it/unitn/disi/joney/MainActivity.java
+++ app/src/main/java/it/unitn/disi/joney/MainActivity.java
@@ -1,36 +1,66 @@
 package it.unitn.disi.joney;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnLogin, btnSignup;
 
     SQLiteDatabase dbJoney;
-    String dbName = "JoneyDB";
-    String createTableUser  = "CREATE TABLE IF NOT EXISTS User(UserID int NOT NULL, Email varchar(100) NOT NULL UNIQUE, FirstName varchar(50) NOT NULL, LastName varchar(50) NOT NULL, Password varchar(255) NOT NULL, PRIMARY KEY(UserID));";
-    String createTableJob  = "CREATE TABLE IF NOT EXISTS Job(JobID int NOT NULL, Title varchar(50) NOT NULL, Description varchar(1000), Completed boolean NOT NULL, Latitude float NOT NULL, Longitude float NOT NULL, Category int, AuthorID int NOT NULL, WorkerID int, PRIMARY KEY(JobID), FOREIGN KEY (Category) REFERENCES JobCategory(JCID), FOREIGN KEY (AuthorID) REFERENCES User(UserID), FOREIGN KEY (WorkerID) REFERENCES User(UserID));";
-    String createTableFeedback  = "CREATE TABLE IF NOT EXISTS Feedback(FBID int NOT NULL, Rating int NOT NULL, Comment varchar(1000), AuthorID int NOT NULL, ReceiverID int NOT NULL, PRIMARY KEY(FBID), FOREIGN KEY (AuthorID) REFERENCES User(UserID), FOREIGN KEY (ReceiverID) REFERENCES User(UserID));";
-    String createTableJobCandidate  = "CREATE TABLE IF NOT EXISTS JobCandidate(JobID int NOT NULL, CandidateID int NOT NULL, PRIMARY KEY (JobID,CandidateID), FOREIGN KEY (JobID) REFERENCES Job(JobID), FOREIGN KEY (CandidateID) REFERENCES User(UserID));";
-    String createTableJobCategory  = "CREATE TABLE IF NOT EXISTS JobCategory(JCID int NOT NULL, Name varchar(30) NOT NULL, Description varchar(1000), PRIMARY KEY(JCID));";
-    String createTableJobImage  = "CREATE TABLE IF NOT EXISTS JobImage(Source varchar(255) NOT NULL, JobID int NOT NULL, PRIMARY KEY (Source), FOREIGN KEY (JobID) REFERENCES Job(JobID));";
-    String createTableTicket  = "CREATE TABLE IF NOT EXISTS Ticket(TicketID int NOT NULL, JobID int NOT NULL, Issue varchar(1000) NOT NULL, PRIMARY KEY (TicketID), FOREIGN KEY (JobID) REFERENCES Job(JobID));";
-    String createTableTicketImage  = "CREATE TABLE IF NOT EXISTS TicketImage(Source varchar(255) NOT NULL, TicketID int NOT NULL, PRIMARY KEY(Source), FOREIGN KEY (TicketID) REFERENCES Ticket(TicketID));";
-
+    Constants c = Constants.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //fixing view when keyboard appear
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
+        createDatabase();
+
         btnLogin = (Button) findViewById(R.id.btn_login);
         btnSignup = (Button) findViewById(R.id.btn_signup);
 
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, password, dbPassword;
+                EditText etEmail, etPassword;
+
+                etEmail = (EditText) findViewById(R.id.email);
+                email = etEmail.getText().toString();
+                etPassword = (EditText) findViewById(R.id.password);
+                password = etPassword.getText().toString();
+
+                String checkUser = "SELECT Password FROM USER WHERE Email='"+email+"';";
+                SQLiteDatabase dbJoney = openOrCreateDatabase(c.getDbName(),MODE_PRIVATE,null);
+
+
+                Cursor resultSet = dbJoney.rawQuery(checkUser,null);
+                resultSet.moveToFirst();
+                if(resultSet.getCount() != 0)
+                {
+                    dbPassword = resultSet.getString(0);
+                    password = c.md5(password);
+                    if (password.equals(dbPassword))
+                        Toast.makeText(getApplicationContext(), "Login ok!", Toast.LENGTH_SHORT).show();
+                    else
+                        Toast.makeText(getApplicationContext(), "Wrong password!", Toast.LENGTH_SHORT).show();
+                }
+                else
+                    Toast.makeText(getApplicationContext(), "Account does not exist!", Toast.LENGTH_SHORT).show();
+            }
+        });
         btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,11 +69,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
     }
 
     public void createDatabase()
     {
-        dbJoney = openOrCreateDatabase(dbName,MODE_PRIVATE,null);
+        dbJoney = openOrCreateDatabase(c.getDbName(),MODE_PRIVATE,null);
+        dbJoney.execSQL(c.getCreateTableUser());
+        dbJoney.execSQL(c.getCreateTableJobCategory());
+        dbJoney.execSQL(c.getCreateTableJob());
+        dbJoney.execSQL(c.getCreateTableJobImage());
+        dbJoney.execSQL(c.getCreateTableJobCandidate());
+        dbJoney.execSQL(c.getCreateTableFeedback());
+        dbJoney.execSQL(c.getCreateTableTicket());
+        dbJoney.execSQL(c.getCreateTableTicketImage());
+
+        Toast.makeText(getApplicationContext(), "Database created!", Toast.LENGTH_SHORT).show();
     }
 
 
--- app/src/main/java/it/unitn/disi/joney/SignupActivity.java
+++ app/src/main/java/it/unitn/disi/joney/SignupActivity.java
@@ -1,24 +1,76 @@
 package it.unitn.disi.joney;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class SignupActivity extends AppCompatActivity {
 
-
     Button btnSignup, btnLogin;
+    Constants c = Constants.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        //fixing view when keyboard appear
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
         btnSignup = (Button) findViewById(R.id.btn_signup);
         btnLogin = (Button) findViewById(R.id.btn_login);
 
+        //Signup Button create a new entry in the database
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String firstName, lastName, email, password, repeatPassword;
+                EditText etFirstName, etLastName, etEmail, etPassword, etRepeatPassword;
+
+                etFirstName = (EditText) findViewById(R.id.first_name);
+                firstName = etFirstName.getText().toString();
+                etLastName = (EditText) findViewById(R.id.last_name);
+                lastName = etLastName.getText().toString();
+                etEmail = (EditText) findViewById(R.id.email);
+                email = etEmail.getText().toString();
+                etPassword = (EditText) findViewById(R.id.password);
+                password = etPassword.getText().toString();
+                etRepeatPassword = (EditText) findViewById(R.id.repeat_password);
+                repeatPassword = etRepeatPassword.getText().toString();
+
+                if(isValidEmail(email))
+                {
+                    if(password.equals(repeatPassword))
+                    {
+                        password = c.md5(password);
+
+                        String createUser = "INSERT INTO User(Email, FirstName, LastName, Password) VALUES('"+email+"','"+firstName+"','"+lastName+"','"+password+"');";
+                        SQLiteDatabase dbJoney = openOrCreateDatabase(c.getDbName(),MODE_PRIVATE,null);
+
+                        dbJoney.execSQL(createUser);
+                        Toast.makeText(getApplicationContext(), "Account created!", Toast.LENGTH_SHORT).show();
+                        Intent intLogin = new Intent(SignupActivity.this,MainActivity.class);
+                        startActivity(intLogin);
+                    }
+                    else
+                        Toast.makeText(getApplicationContext(), "Passwords are not the same!", Toast.LENGTH_SHORT).show();
+                }
+                else
+                    Toast.makeText(getApplicationContext(), "Not valid Email Address!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        //Login Button opens back the Login Activity
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,4 +79,10 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
     }
+
+    //Check Email validity
+    public static boolean isValidEmail(CharSequence target) {
+        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
+    }
+
 }
