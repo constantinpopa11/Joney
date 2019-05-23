d4008b8a587f0e105680ab7e2f8821da3bdf4087
Constantin <constantin.popa@studenti.unitn.it>
2019-05-23 16:58:04 +0200

    add drawer navigation

--- app/src/main/java/it/unitn/disi/joney/LoginActivity.java
+++ app/src/main/java/it/unitn/disi/joney/LoginActivity.java
@@ -1,28 +1,19 @@
 package it.unitn.disi.joney;
 
-import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteStatement;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
 
 public class LoginActivity extends AppCompatActivity {
 
